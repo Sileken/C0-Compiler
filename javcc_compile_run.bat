e5f96eaf53b73f3bacb29da3100447b7c1d2f413
Silvan <silvan-hau@web.de>
2017-05-18 21:34:27 +0200

    Finalized build script for vs-code. Use "Ctrl+Shift+B" to run the batch file, which compiles the .jj file and generates the parser... Current setup: Read input from a FILE (set to different file in the batch-file). You can also read from stdin, see bat file

--- javcc_compile_run.bat
+++ javcc_compile_run.bat
@@ -1,12 +1,17 @@
 @echo off
 
+rem filename for the C0-INPUT-FILE. Set to NOTHING if you want to use console input
+SET c0_file=example.c0
+
+if defined c0_file (SET c0_dir=../C0Code/%c0_file%) else (SET c0_dir=)
+
 rem Parser Name, specified in .jj
 SET parser_name=C0Parser
 rem output directory
-SET out_dir=bin
+SET out_dir=compiler/bin
 SET current_path=%~dp0
 
-rem Check for parameter
+rem Check for parameter (Silvan: Not really needed anymore right?)
 IF [%1] == [] SET /P compiler_file="Enter a file name: "
 IF [%compiler_file%] == [] SET compiler_file=%1
 
@@ -15,8 +20,9 @@ call javacc -output_directory:%out_dir% %compiler_file%
 
 cd %out_dir%
 call javac %parser_name%.java
-call java %parser_name%
-cd %current_path%
+call java %parser_name% %c0_dir%
 
-cmd.exe /k
-exit
+rem Benny's old code. Not needed anymore. Just kept it for whatever reason.
+rem cd %current_path% 
+rem cmd.exe /k
+rem exit
