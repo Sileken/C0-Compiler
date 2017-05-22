6d9598516ed2f731b9d02e49f45432cda92045a5
Patrick Hasenauer <phasenauer@agile-is.de>
2017-05-22 21:46:30 +0200

    Changed Task Execution Policy

--- taskManager.ps1
+++ taskManager.ps1
@@ -7,11 +7,11 @@ Write-Host "Running task $($task)";
 
 if($task -eq "build"){
     &.\build.ps1;
-    invoke-expression 'cmd /c start powershell -Command { &.\run.ps1 -mainFileName C0Compiler.class }'
+    start-process powershell.exe -argument '-noprofile -executionpolicy unrestricted -noexit -command .\run.ps1 -mainFileName C0Compiler.class'
 } elseif($task -eq "test"){
     &.\build.ps1;
     #todo: run test files
     #invoke-expression 'cmd /c start powershell -Command { &.\run.ps1 -mainFileName C0Compiler.class }'
 } elseif($task -eq "run"){
-    invoke-expression 'cmd /c start powershell -Command { &.\run.ps1 -mainFileName C0Compiler.class }'
+    start-process powershell.exe -argument '-noprofile -executionpolicy unrestricted -noexit -command .\run.ps1 -mainFileName C0Compiler.class '
 }
