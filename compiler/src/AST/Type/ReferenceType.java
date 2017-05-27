7bba9a87b6a5c0714436072ebe89c185a6d56ffd
Patrick Hasenauer <phasenauer@agile-is.de>
2017-05-27 23:30:41 +0200

    Improved AST for Types

--- compiler/src/AST/Type/ReferenceType.java
+++ compiler/src/AST/Type/ReferenceType.java
@@ -1,4 +1,10 @@
 package ast.type;
 
 public class ReferenceType extends Type {
+    private Type innerType;
+
+    public ReferenceType(Type innerType){
+        this.innerType = innerType;
+        this.addChild(this.innerType);
+    }
 }
