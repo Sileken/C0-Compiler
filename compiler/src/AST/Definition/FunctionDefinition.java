d3d087514123c65d013ee2764f0b5bfe7479db0f
Patrick Hasenauer <phasenauer@agile-is.de>
2017-05-28 16:13:43 +0200

    Added Statements to function declaration;

--- compiler/src/AST/Definition/FunctionDefinition.java
+++ compiler/src/AST/Definition/FunctionDefinition.java
@@ -3,13 +3,15 @@ package ast.definition;
 import java.util.ArrayList;
 import java.util.List;
 import ast.identifier.*;
+import ast.statement.*;
 import ast.type.*;
 
 public class FunctionDefinition extends Definition {
   private Type returnType;
   private List<ParameterDefinition> parameterDefs = new ArrayList<ParameterDefinition>();
+  private List<Statement> statements = new ArrayList<Statement>();
 
-  public FunctionDefinition(Type returnType, VariableFunctionIdentifier funcId, List<ParameterDefinition> parameterDefs){
+  public FunctionDefinition(Type returnType, VariableFunctionIdentifier funcId, List<ParameterDefinition> parameterDefs, List<Statement> statements){
     super(funcId);
     this.returnType = returnType;
     this.addChild(this.returnType);
@@ -17,6 +19,11 @@ public class FunctionDefinition extends Definition {
     if(parameterDefs != null && !parameterDefs.isEmpty()){
         this.parameterDefs = parameterDefs;
         this.addChilds(this.parameterDefs);
+    }
+
+    if(statements != null && !statements.isEmpty()){
+        this.statements = statements;
+        this.addChilds(this.statements);
     }
   }
 }
