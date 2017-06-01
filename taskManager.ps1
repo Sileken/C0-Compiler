92fb15717047eb4ccf902139461bd1d36a7cb1b7
Patrick Hasenauer <phasenauer@agile-is.de>
2017-06-01 20:57:03 +0200

    "Improved" Expression Production and Improved build scripts

--- taskManager.ps1
+++ taskManager.ps1
@@ -4,23 +4,27 @@ Param(
 );
 
 $mainFileName = "C0Compiler.class";
-$userTestFile = "C0Code/own.c0";
+$userTestFile = "C0Code/example4.c0";
 $allTestFileFolder = "C0Code";
 
 Write-Host "Start running task $($task)" -foreground "green";
 
 if($task -eq "build"){
+    clear;
     &.\build.ps1;
     &.\run.ps1 -mainFileName $mainFileName;
 } elseif($task -eq "test"){
+    clear;
     &.\build.ps1;
     $testfiles = Get-ChildItem $allTestFileFolder -Recurse | ?{ !$_.PSIsContainer }
     &.\run.ps1 -mainFileName $mainFileName -testFiles $testfiles;
 } elseif($task -eq "testown"){
+    clear;
     &.\build.ps1;
     $testfiles = Get-Item $userTestFile | ?{ !$_.PSIsContainer }
     &.\run.ps1 -mainFileName $mainFileName -testFiles $testfiles;
 } elseif($task -eq "run"){
+    clear;
     &.\run.ps1 -mainFileName $mainFileName;
 }
 
