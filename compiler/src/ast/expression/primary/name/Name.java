ba94a5a71991061ceddaa0769cba0e5576e05b0b
Patrick Hasenauer <phasenauer@agile-is.de>
2017-06-18 23:28:44 +0200

    Removed Declaration Symbol Info from Identifier and added this info to primary.name;

--- compiler/src/ast/expression/primary/name/Name.java
+++ compiler/src/ast/expression/primary/name/Name.java
@@ -1,9 +1,21 @@
 package ast.expression.primary.name;
 
 import ast.expression.primary.*;
+import symboltable.*;
 
 public abstract class Name extends Primary {
+    protected Symbol originalDeclaration;
+
     public Name() {
         super();
     }
+    
+    public Symbol getOriginalDeclaration() {
+		return originalDeclaration;
+	}
+
+	public void setOriginalDeclaration(Symbol originalDeclaration) {
+		this.originalDeclaration = originalDeclaration;
+	}
+
 }
--- compiler/src/ast/identifier/Identifier.java
+++ compiler/src/ast/identifier/Identifier.java
@@ -6,7 +6,6 @@ import parser.*;
 
 public abstract class Identifier extends ASTNode {
     private final Token idToken;
-    private Identifier declarationNode;
     private Type type;
 
     public Identifier(Token idToken) {
@@ -17,21 +16,5 @@ public abstract class Identifier extends ASTNode {
 
     public String getName() {
         return this.idToken.image;
-    }
-
-    public void setDeclarationNode(Identifier declarationNode) {
-        this.declarationNode = declarationNode;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
-
-    public Identifier getDeclarationNode() {
-        return this.declarationNode;
-    }
-
-    public Type getType() {
-        return this.type;
     }
 }
