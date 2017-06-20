65d9b964ec23a88d5251d189d41aeb2ba6658c52
Silvan <silvan-hau@web.de>
2017-06-20 11:31:26 +0200

    moved the prefix into the suffix-list in PrimaryExpression() and in addSuffixed() in Primary.java

--- compiler/src/ast/expression/primary/Primary.java
+++ compiler/src/ast/expression/primary/Primary.java
@@ -11,7 +11,7 @@ public abstract class Primary extends Expression {
 
     // Build up the suffixes tree in java-code
     // This function gets recursively called.
-    public void addSuffixes(Primary prefix, List<Primary> suffixes)
+    public void addSuffixes(List<Primary> suffixes)
     {
         // Delete last element which is this element itself
         if(suffixes.size() >= 1)
@@ -27,13 +27,7 @@ public abstract class Primary extends Expression {
             this.addChild(nextSuffix);
 
             // Recursive call
-            nextSuffix.addSuffixes(prefix, suffixes);
-        }
-        else
-        {
-            // The prefix which is the first elem has to be added as the last child.
-            // If the suffix list is empty we reached the end, so add the prefix now as a child.
-            this.addChild(prefix);
+            nextSuffix.addSuffixes(suffixes);
         }
 
     }
