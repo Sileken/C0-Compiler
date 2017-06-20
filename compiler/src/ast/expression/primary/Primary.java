f34708ebf224b27f5cd8ee93431874207baf26b8
Patrick Hasenauer <phasenauer@agile-is.de>
2017-06-20 13:19:31 +0200

    Added Set Prefix private function in primary class and renamed addSuffixes to addPrefixes.

--- compiler/src/ast/expression/primary/Primary.java
+++ compiler/src/ast/expression/primary/Primary.java
@@ -5,30 +5,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Primary extends Expression {
+    Primary prefix = null;
+
     public Primary() {
         super();
     }
 
-    // Build up the suffixes tree in java-code
+    private void setPrefix(Primary prefix) {
+        this.prefix = prefix;
+        this.addChild(prefix);
+    }
+
+    public Primary getPrefix() {
+        return this.prefix;
+    }
+
+    // Build up the prefixes tree in java-code
     // This function gets recursively called.
-    public void addSuffixes(List<Primary> suffixes)
-    {
+    public void addPrefixes(List<Primary> prefixes) {
         // Delete last element which is this element itself
-        if(suffixes.size() >= 1)
-        {
-            suffixes.remove(suffixes.size() - 1);
+        if (prefixes.size() >= 1) {
+            prefixes.remove(prefixes.size() - 1);
         }
 
-        // If the suffix-list is not empty add the next suffix as a child and call this func resursively
-        if(!suffixes.isEmpty())
-        {
-            // Get the next suffix which is the last element now
-            Primary nextSuffix = suffixes.get(suffixes.size() - 1);
-            this.addChild(nextSuffix);
+        // If the prefix-list is not empty add the next prefix as a child and call this func resursively
+        if (!prefixes.isEmpty()) {
+            // Get the next prefix which is the last element now
+            Primary nextPrefix = prefixes.get(prefixes.size() - 1);
+            this.setPrefix(nextPrefix);
 
             // Recursive call
-            nextSuffix.addSuffixes(suffixes);
+            nextPrefix.addPrefixes(prefixes);
         }
-
     }
 }
