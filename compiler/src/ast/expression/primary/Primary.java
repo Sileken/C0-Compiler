021c4fd531d751f9449e57b3d5692cb5d4d931a3
Patrick Hasenauer <phasenauer@agile-is.de>
2017-07-15 17:25:47 +0200

    Removed unused code

--- compiler/src/ast/expression/primary/Primary.java
+++ compiler/src/ast/expression/primary/Primary.java
@@ -20,26 +20,7 @@ public abstract class Primary extends Expression {
     public Primary getPrefix() {
         return this.prefix;
     }
-
-    // Build up the prefixes tree in java-code
-    // This function gets recursively called.
-    public void addPrefixes(List<Primary> prefixes) {
-        // Delete last element which is this element itself
-        if (prefixes.size() >= 1) {
-            prefixes.remove(prefixes.size() - 1);
-        }
-
-        // If the prefix-list is not empty add the next prefix as a child and call this func resursively
-        if (!prefixes.isEmpty()) {
-            // Get the next prefix which is the last element now
-            Primary nextPrefix = prefixes.get(prefixes.size() - 1);
-            this.setPrefix(nextPrefix);
-
-            // Recursive call
-            nextPrefix.addPrefixes(prefixes);
-        }
-    }
-
+    
     @Override
     public int countArithmeticOps()
     {
